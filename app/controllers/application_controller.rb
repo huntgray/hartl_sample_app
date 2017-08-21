04f4c6f6fd817acded3542528b88e6cb63f54c4d
Benjamin Hunt Gray <benjamin.gray@yahoo.com>
2017-08-21 13:35:34 -0400

    add index route to config routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def index
+    render html: "This is the Hartl Sample App temporary Home Page"
+  end
+
 end
