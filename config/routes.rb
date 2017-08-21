83bf6a03a9887d1b738b4ecf4284c5ad6a1ef24a
Benjamin Hunt Gray <benjamin.gray@yahoo.com>
2017-08-21 18:30:52 -0400

    change get / route from static_pages/home to static_pages#home

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
 
-  get '/', to: 'static_pages/home'
-
   get 'static_pages/home'
   get 'static_pages/help'
   get 'static_pages/about'
@@ -9,8 +7,8 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  # root 'static_pages#home'
+  root 'static_pages#home'
 
-  # get '/', to: 'application#index'
+  get '/', to: 'static_pages#home'
 
 end
